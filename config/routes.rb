bac8a311f32f771e4d73f5eccf8c671ff00c2181
Patrick Wiseman <patrick@wiserstudios.com>
2014-11-24 09:45:16 -0500

    Update scoreboard route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'scoreboard/show'
   get 'js_tests/index'
   root "home#index"
   get "/jam_timer", to: "home#jam_timer"
+  get "/scoreboard", to: "scoreboard#show"
 end
