f34a1ea45d2cc4eaab2f1d66f59ebafb0561684c
Dylan Egan <dylanegan@gmail.com>
2014-12-05 14:46:40 +0100

    Weird indenting. :v:

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 ENV["RAILS_ENV"] = "test"
+
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
@@ -15,9 +16,9 @@ Dir.glob(File.dirname(__FILE__) + "/support/**/*.rb").each do |file|
 end
 
 class ActiveSupport::TestCase
-    ActiveRecord::Migration.check_pending!
+  ActiveRecord::Migration.check_pending!
 
-    # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
