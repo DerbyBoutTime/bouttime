846d51de2232503cb3e2b1f3abbbc84f6191acdb
Patrick Wiseman <patrick@wiserstudios.com>
2014-12-08 06:14:58 -0500

    Add routes for stubbed out paths

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   root "home#jam_timer"
   get "/jam_timer", to: "home#jam_timer"
   get "/scoreboard", to: "home#scoreboard"
+  get "/lineup_tracker", to: "home#lineup_tracker"
+  get "/penalty_box", to: "home#penalty_box"
+  get "/penalty_tracker", to: "home#penalty_tracker"
+  get "/scorekeeper", to: "home#scorekeeper"
+  get "/penalty_whiteboard", to: "home#penalty_whiteboard"
+  get "/announcer", to: "home#announcer"
+  get "/global_bout_notes", to: "home#global_bout_notes"
 end
