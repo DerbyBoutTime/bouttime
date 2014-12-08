52c6dbac7fa52feee15dc9c3ef4efd90effd3d20
Patrick Wiseman <patrick@wiserstudios.com>
2014-12-08 06:32:28 -0500

    Add navigation by default with opt-out option for full screen displays, stub out react components

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,6 +21,7 @@ class HomeController < ApplicationController
 
   def scoreboard
     @active_class = "scoreboard"
+    @hide_nav = true
     @home_team_logo_url = params[:home_team_logo_url] || placehold_it(600, 600)
     @away_team_logo_url = params[:away_team_logo_url] || placehold_it(600, 600)
     @home_team_name = params[:home_team_name].to_s || "Atlanta Rollergirls"
@@ -29,10 +30,12 @@ class HomeController < ApplicationController
 
   def penalty_whiteboard
     @active_class = "penalty_whiteboard"
+    @hide_nav = true
   end
 
   def announcer
     @active_class = "announcer"
+    @hide_nav = true
   end
 
   def global_bout_notes
