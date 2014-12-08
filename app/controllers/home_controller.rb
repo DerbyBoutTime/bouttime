9298b5874759b0cc088310f3d2732b17711fffa5
Patrick Wiseman <patrick@wiserstudios.com>
2014-12-08 06:12:29 -0500

    Move all the NSO components to the home controller, set active class

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,47 @@
 class HomeController < ApplicationController
-  def index
-
+  def jam_timer
+    @active_class = "jam_timer"
   end
 
-  def jam_timer
+  def lineup_tracker
+    @active_class = "lineup_tracker"
+  end
+
+  def penalty_box
+    @active_class = "penalty_box"
+  end
+
+  def penalty_tracker
+    @active_class = "penalty_tracker"
+  end
+
+  def scorekeeper
+    @active_class = "scorekeeper"
+  end
+
+  def scoreboard
+    @active_class = "scoreboard"
+    @home_team_logo_url = params[:home_team_logo_url] || placehold_it(600, 600)
+    @away_team_logo_url = params[:away_team_logo_url] || placehold_it(600, 600)
+    @home_team_name = params[:home_team_name].to_s || "Atlanta Rollergirls"
+    @away_team_name = params[:away_team_name].to_s || "Gotham Rollergirls"
+  end
+
+  def penalty_whiteboard
+    @active_class = "penalty_whiteboard"
+  end
+
+  def announcer
+    @active_class = "announcer"
+  end
+
+  def global_bout_notes
+    @active_class = "global_bout_notes"
+  end
+
+  private
+
+  def placehold_it(width, height, text = "")
+    "http://placehold.it/#{width}x#{height}&text=#{text}+(#{width}x#{height})"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'scoreboard/show'
   get 'js_tests/index'
-  root "home#index"
+  root "home#jam_timer"
   get "/jam_timer", to: "home#jam_timer"
-  get "/scoreboard", to: "scoreboard#show"
+  get "/scoreboard", to: "home#scoreboard"
 end
